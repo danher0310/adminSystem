d5df416c7dc83ff094c1f1e43f69ff92611957a7
Daniel <daniel.herrera2292@gmail.com>
2023-01-26 20:17:59 -0400

    Working in navbar

--- front/src/components/navigations/Navbar.js
+++ front/src/components/navigations/Navbar.js
@@ -1,15 +1,37 @@
 import React from 'react'
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import { logout } from '../../redux/actions/authActions';
+import { useDispatch } from 'react-redux';
 
 const  NavigationBar = () => {
+
+  const publicLinks = () => {
+
+  };
+
+  const authLinks = () =>{
+
+  };
+
   return (
     <div>
-       <Navbar bg="light">
-        <Container>
-          <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+      <Container>
+          <Navbar bg="light" expand="lg">
+            <Container className="mx-4 px-4">
+              <Navbar.Brand href="#home">AdminSystem</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto">
+                  <Nav.Link href="#home">Home</Nav.Link>
+                  <Nav.Link href="#link">Link</Nav.Link>
+                  <Nav.Link href="#link">Link</Nav.Link>
+                
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
         </Container>
-      </Navbar>
-    </div>
+      </div>
   )
 }
 
